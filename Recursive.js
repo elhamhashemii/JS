2ff806447a120b75b7103004588e64c01d7589de
elhamhashemi <elham.aeri17@gmail.com>
2022-01-11 15:26:18 +0330

    recursive funstions added

--- Recursive.js
+++ Recursive.js
@@ -1,19 +1,34 @@
 function fib(n) {
   if (n == 1 || n == 2) return 1;
-    else return fib(n - 1) + fib(n - 2)
+  else return fib(n - 1) + fib(n - 2);
 }
 
 function fact(n) {
-    if (n == 0 || n == 1) return 1;
-    else return fact(n - 1)* n
+  if (n == 0 || n == 1) return 1;
+  else return fact(n - 1) * n;
 }
 
-function findMaximum() {
-  console.log("findMax");
+function findMaximum(arr) {
+  if (arr.length == 1) return arr[0];
+  let max = findMaximum(arr.slice(1));
+  if (max > arr[0]) {
+    return max;
+  } else {
+    return arr[0];
+  }
 }
 
-function findSum() {
-  console.log("findSum");
+function findSum(arr) {
+  if (arr.length == 1) return arr[0];
+  let sum = findSum(arr.slice(1))
+  return sum + arr[0]
 }
+
+
+let testArr = [5, 3, 10, 39, 75, 8];
+
+var span = document.createElement("span");
+span.appendChild(document.createTextNode(findSum(testArr)));
+document.body.appendChild(span);
 
 // fib()
