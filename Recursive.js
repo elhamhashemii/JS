6bfd6588c5035b0716756c84bfa8ac6db7d99081
elhamhashemi <elham.aeri17@gmail.com>
2022-01-12 16:56:20 +0330

    quick sort added

--- Recursive.js
+++ Recursive.js
@@ -24,68 +24,9 @@ function findSum(arr) {
   return sum + arr[0];
 }
 
-function swap(arr, i, j) {
-  i = 0;
-  for (j = i + 1; j < arr.length; j++) {
-    let temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-  }
-  return arr;
-}
 
-function merge(arr1, arr2) {
-  // let i = 0;
-  // let j = 0;
-  let mergedArr = [];
-  // while (i < arr1.length && j < arr2.length) {
-  //   if (arr1[i] < arr2[j]) {
-  //     mergedArr.push(arr1[i]);
-  //     i++;
-  //   } else {
-  //     mergedArr.push(arr2[j]);
-  //     j++;
-  //   }
-  //   while (arr1.length) {
-  //     mergedArr.push(arr1[i])
-  //     i++
-  //   }
-  //   while (arr2.length) {
-  //     mergedArr.push(arr2[j])
-  //     j++
-  //   }
-  // }
-  // return mergedArr;
-  while (arr1.length && arr2.length) {
+// let testArr = [3, 39, 10, 1, 5, 75, 8]
 
-      mergedArr.push(
-        arr1[0] < arr2[0] ? arr1.shift() : arr2.shift()
-      );
-    }
-    return [...mergedArr, ...arr1, ...arr2];
-  }
-
-function mergeSort(mainArr) {
-  if (mainArr.length == 1) return mainArr;
-  // todo: find the element in the middle of array
-  let indexInTheMiddleOfArr = Math.round(mainArr.length / 2);
-  // todo: make two new array from main arr using "indexInTheMiddleOfArr"
-  let rightArr = mainArr.slice(0, indexInTheMiddleOfArr);
-  let leftArr = mainArr.slice(indexInTheMiddleOfArr, mainArr.length);
-  return merge(mergeSort(leftArr), mergeSort(rightArr));
-  // todo: do it recursively to have arrays with only 1 element
-  // todo: move this one element arrays and merge them together
-  // let a = mergeSort(arr.slice(indexInTheMiddleOfArr));
-  // if (a > arr[0]) {
-  //   swap(arr, a, arr[0])
-  // }
-  // return arr
-}
-
-let testArr = [3, 39, 10, 1, 5, 75, 8];
-let firstArr = [3, 39, 10];
-let secondArr = [1, 5, 75, 8];
-
-var span = document.createElement("span");
-span.appendChild(document.createTextNode(mergeSort(testArr)));
-document.body.appendChild(span);
+var p = document.createElement("p");
+p.appendChild(document.createTextNode(findSum(testArr)));
+document.body.appendChild(p);
